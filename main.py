2cdda774dec5c3780f5811f03ebb4d3eded61ad9
unknown <ahmalbilal@gmail.com>
2024-10-05 12:02:57 +0200

    tttttttt

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ fichier = open("tableau.txt", "r")
 
 tab = fichier.readlines()
 for line in tab :
-    line = line.replace("\n", "").split(" ")
     sommets.append(line.replace("\n", "").split(" "))
     
 print(sommets)
