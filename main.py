6736bef30872907c525bb0c615f191697a24ca21
unknown <ahmalbilal@gmail.com>
2024-10-05 12:02:22 +0200

    tttttttt

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello zedenfefnzefefcze")
+sommets = []
+fichier = open("tableau.txt", "r")
+
+tab = fichier.readlines()
+for line in tab :
+    line = line.replace("\n", "").split(" ")
+    sommets.append(line.replace("\n", "").split(" "))
+    
+print(sommets)
