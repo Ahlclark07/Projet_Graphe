5a191f83d0871b6872bd9df50a92f63eaeb57e72
unknown <ahmalbilal@gmail.com>
2024-10-05 12:18:01 +0200

    tttttttt

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ fichier = open("tableau.txt", "r")
 
 tab = fichier.readlines()
 for line in tab :
-    sommets.append(line.replace("\n", "").split(" "))
-    
-print(sommets)
+    line_intermidiaire = line.replace("\n", "").split(" ")
+    for i in range(0, len(line_intermidiaire)):
+        try:
+            line_intermidiaire[i] = int(line_intermidiaire[i])
+        except:
+            line_intermidiaire.pop(i)
+
+    sommets.append(line_intermidiaire)
+
+
+# print(sommets)
